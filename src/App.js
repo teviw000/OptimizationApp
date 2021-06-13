f70d05c2e65f18a78a1af955b0aa53c37bfc7ab5
teviw000 <31836504+teviw000@users.noreply.github.com>
2021-06-13 00:22:03 -0500

    Found out that the  ReactDOM is re-rendered everytime the state changes, props changes, and context changes would result in component being redone. In realDOM, only div and <p> will change.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { useState } from "react";
 
-import './App.css';
+import Button from './components/UI/Button/Button';
+
+import "./App.css";
 
 function App() {
+  const [showParagraph, setShowParagraph] = useState(false);
+
+  const toggleParagraphHandler = (event) => {
+    setShowParagraph(prevShowParagraph => !prevShowParagraph);
+  }
+
   return (
     <div className="app">
       <h1>Hi there!</h1>
+      {showParagraph && <p>This is new!</p>}
+      <Button onClick={toggleParagraphHandler}>Toggle Paragraph</Button>
     </div>
   );
 }
