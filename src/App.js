f896394c1e77fb4f25f1070d0bb5d3b7bd40a4d6
teviw000 <31836504+teviw000@users.noreply.github.com>
2021-06-15 17:10:10 -0500

    demo'd useCallback

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
-import React, { useState } from "react";
+import React, { useState, useCallback } from "react";
 
-import Button from './components/UI/Button/Button';
+import Button from "./components/UI/Button/Button";
+import DemoOutput from "./components/Demo/DemoOutput";
 
 import "./App.css";
 
 function App() {
   const [showParagraph, setShowParagraph] = useState(false);
 
-  const toggleParagraphHandler = (event) => {
-    setShowParagraph(prevShowParagraph => !prevShowParagraph);
-  }
+  const toggleParagraphHandler = useCallback(() => {
+    setShowParagraph((prevShowParagraph) => !prevShowParagraph);
+  }, []);
 
   return (
     <div className="app">
       <h1>Hi there!</h1>
-      {showParagraph && <p>This is new!</p>}
+      <DemoOutput show={showParagraph} />
       <Button onClick={toggleParagraphHandler}>Toggle Paragraph</Button>
     </div>
   );
